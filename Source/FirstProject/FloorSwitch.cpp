a898b04f30b5d40eba6f0bb881e8049e00bbe1ab
AronT <aron.thompson@racunarstvo.hr>
2021-04-23 17:48:22 +0200

    Added FloorSwitch debug version

---
+++ Source/FirstProject/FloorSwitch.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloorSwitch.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFloorSwitch::AFloorSwitch()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	RootComponent = TriggerBox;
+
+
+	// This is equal as setting in an editor collision presets. I can check all I need there to know what to do here
+	// checks only for collision, no physics stuff. Basically for overlap etc.
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// change obj type
+	TriggerBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	// Disable all response channels and then set it to respond only to pawn overlap
+	TriggerBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	TriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	// Collider size
+	TriggerBox->SetBoxExtent(FVector(62.f,62.f, 32.f));
+	
+	FloorSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSwitch"));
+	FloorSwitch->SetupAttachment(GetRootComponent());
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AFloorSwitch::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapBegin);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapEnd);
+}
+
+// Called every frame
+void AFloorSwitch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap Begin"));
+}
+
+void AFloorSwitch::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap End"));
+}
+
